420c2c80e1f62e4711916f71e6a50bd9819a8c86
AATFAOUI Aya <129607003+aya20053@users.noreply.github.com>
2024-01-01 00:11:16 +0100

    commit

---
+++ footer.php
@@ -0,0 +1,33 @@
+<head>
+<link rel="stylesheet" href="pro.css">
+<style>
+      .email {
+    color:#935d95;
+    text-decoration:none;
+
+}
+</style>
+</head>
+<body>
+    
+
+<footer>
+        <section class="foot">
+          <h4>  A propos de nous</h4>
+          <p> "Votre source de mignonnerie préférée : tout ce qui est mignon en un seul endroit."</p>
+
+
+
+
+      <div class="icones">
+      <i class="fa fa-envelope-o"></i>
+                               <a class="email" href="mailto:ayaaatfaoui38@gmail.com"> <span> ayaaatfaoui38@gmail.com  /</span></a>
+                              <a class="email" href="mailto:zahraechokrii@gmail.com"><span> zahraechokrii@gmail.com</span></a>
+                                     
+       <p>made with <i class="fa fa-superpowers" ></i> by  Chokri Zahra, Aatfaoui Aya .</p>
+                              </span> 
+                        
+      
+    </section>
+</footer>
+</body>
